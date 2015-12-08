c99a4352a861754af01160bbe50e9ac7783f8a98
Daniel Neel <dneelyep@gmail.com>
2015-12-08 01:33:25 -0500

    Rename buttons in the camera screen to be more readable.

--- app/src/main/java/com/cecs/uoflcecscapstone/homeaccessibility/CameraActivity.java
+++ app/src/main/java/com/cecs/uoflcecscapstone/homeaccessibility/CameraActivity.java
@@ -9,20 +9,20 @@ import android.widget.Button;
  * Created by Drew on 10/20/2015.
  */
 public class CameraActivity extends Activity implements View.OnClickListener{
-    Button b1=null;
-    Button b2=null;
+    Button LookAtCameraButton = null;
+    Button BackButton = null;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        b1=(Button) findViewById(R.id.button8);
-        b1.setOnClickListener(this);
-        b2=(Button) findViewById(R.id.button19);
-        b2.setOnClickListener(this);
+        LookAtCameraButton = (Button) findViewById(R.id.lookAtCameraButton);
+        LookAtCameraButton.setOnClickListener(this);
+        BackButton = (Button) findViewById(R.id.backButton);
+        BackButton.setOnClickListener(this);
     }
 
     public void onClick(View v) {
-        if(v==b1)
+        if(v == LookAtCameraButton)
         {
         }
         else
